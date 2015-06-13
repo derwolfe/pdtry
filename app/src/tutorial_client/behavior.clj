3d08856098ee7237b427a22afcd1a6410f64432c
derwolfe <chriswwolfe@gmail.com>
2015-06-13 16:55:48 -0500

    make it possible to increment the count

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -9,7 +9,11 @@
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 (def example-app
   {:version 2
    :transform [[:inc [:my-counter] inc-transform]]
-   :emit [[#{[:*]} (app/default-emitter [:main])]]})
+   :emit [{:init init-main}
+          [#{[:*]} (app/default-emitter [:main])]]})
