ced8d87ad9c4dcedb63cae55541c9a131cd7816b
derwolfe <chriswwolfe@gmail.com>
2015-06-13 21:56:35 -0500

    starting to work with renderers, several keys aren't showing up. why?

--- app/src/tutorial_client/simulated/start.cljs
+++ app/src/tutorial_client/simulated/start.cljs
@@ -1,11 +1,12 @@
 (ns tutorial-client.simulated.start
   (:require [io.pedestal.app.render.push.handlers.automatic :as d]
             [tutorial-client.start :as start]
-            [tutorial-client.rendering :as rendering]
             [io.pedestal.app.protocols :as p]
             [io.pedestal.app :as app]
             [tutorial-client.simulated.services :as services]
+            [tutorial-client.rendering :as rendering]
             [goog.Uri]
+            [goog.Uri :as guri]
             ;; This needs to be included somewhere in order for the
             ;; tools to work.
             [io.pedestal.app-tools.tooling :as tooling]))
@@ -14,9 +15,13 @@
   (let [uri (goog.Uri. (.toString  (.-location js/document)))]
     (.getParameterValue uri name)))
 
-
 (defn ^:export main []
-  (let [app (start/create-app d/data-renderer-config)
+  (let [uri (goog.Uri. (.toString (.-location js/document)))
+        renderer (.getParameterValue uri "renderer")
+        render-config (if (= renderer "auto")
+                        d/data-renderer-config
+                        (rendering/render-config))
+        app (start/create-app render-config)
         services (services/->MockServices (:app app))]
     (app/consume-effects (:app app) services/services-fn)
     (p/start services)
